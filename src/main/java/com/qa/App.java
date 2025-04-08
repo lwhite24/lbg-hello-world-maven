233a63972da51fd1266858e6baa531dfc64f47e4
Lee <lee@qa.com>
2025-04-08 11:57:07 +0100

    changed names

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
-        System.out.println(sayHelloToSomeone("Alice"));
-        System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Lee"));
+        System.out.println(sayHelloToSomeone("Suzy"));
+        System.out.println(sayHelloToSomeone("Sadie"));
+        System.out.println(sayHelloToSomeone("Evie"));
         System.out.println(sayGoodbye());
     }
 
